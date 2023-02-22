7ccf62205796dfe7f5ebf6e0242999fe44f4e3a4
IndunilRanganath <indunilranganath071@gmail.com>
2023-02-23 00:19:58 +0530

    post request complected^C

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
 const express = require('express')
 const mongoose = require('mongoose');
+require('dotenv').config();
+const bodyParser = require('body-parser')
 
 mongoose.set('strictQuery', true);
+//==============================
+const UserRoute = require('./routes/UserRoute');
+//==============================
 
 const app = express ();
+const serverPort = process.env.SERVER_PORT;
+
 
 mongoose.connect(
-    'mongodb://localhost:27017/pos'
+    'mongodb://localhost:27017/test'
 ).then(()=>{
-    app.listen(3000, () => {
-        console.log('server started.!')
+    app.listen(serverPort, () => {
+        console.log(`server started & runnning on the port ${serverPort}`);
     })
 })
+
+//==================
+app.use('/api/v1/user', UserRoute)
+//==================
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 
 app.get('/api/v1/test', (req, res) => {
     res.status(200).json({'Message' : 'success'});
     }
 )
+
+
 
